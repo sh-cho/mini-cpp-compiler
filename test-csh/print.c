59f2edbeafa7b770dc69a435d1e48cf94fdf66c9
Seonghyeon Cho <seonghyeoncho96@khu.ac.kr>
2017-11-04 05:58:04 +0900

    yyin

--- test-csh/print.c
+++ test-csh/print.c
@@ -9,6 +9,8 @@ int main() {
 	yydebug = 1;
 
 	// fp = fopen("sample.out", "w");
+	yyin = fpen("test-input", "r");
+
 	if (!yyparse())
 		print();
 	else {
@@ -17,12 +19,16 @@ int main() {
 
 	// fprintf(fp, "\n");
 	// fclose(fp);
+	fclose(yyin);
 	return 0;
 }
 
 void print() {
 	if (prog_head == NULL) {
 		fprintf(stderr, "Program does not exist.\n");
+		return;
+	} else {
+		printf("Program exist\n");
 		return;
 	}
 	//print
