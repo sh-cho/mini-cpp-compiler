dff562257dd9e6f85c9d2063e761d05b38a39f27
Seonghyeon Cho <seonghyeoncho96@khu.ac.kr>
2017-11-04 20:22:54 +0900

    add little print

--- master/print.c
+++ master/print.c
@@ -14,14 +14,28 @@ int main() {
 	if (!yyparse())
 		dfs();
 	
+
+	printf("  *** parsing end ***\n");
 	// fprintf(fp, "\n");
 	// fclose(fp);
 	return 0;
 }
 
 void dfs() {
-	// if (head == NULL) {
-	// 	fprintf(stderr, "Program does not exist.\n");
-	// 	return;
-	// }
+	if (head == NULL) {
+		fprintf(stderr, "Program does not exist.\n");
+		return;
+	}
+
+	if (head->_class == NULL) {
+		printf("  *** There is no Class\n");
+	} else {
+		printf("  *** Class exists\n");
+	}
+
+	if (head->mainFunc == NULL) {
+		printf("  *** there is no main\n");
+	} else {
+		printf("  *** main exists");
+	}
 }
